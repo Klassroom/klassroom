86c373c5ed838e9e7be879c576506dd11dc6c193
Bu Kinoshita <bukinoshita@gmail.com>
2015-10-13 11:51:12 -0400

    Requiring Routes

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ var http  = require('http').createServer(app);
 app.set('views', __dirname + '/client/views/');
 app.set('view engine', 'jade');
 
+app.use('/scripts', express.static(__dirname + '/dist/scripts'));
+app.use('/stylesheets', express.static(__dirname + '/dist/stylesheets'));
+app.use('/images', express.static(__dirname + '/dist/images'));
+
+require('./config/routes')(app);
 
 http.listen(3000, function(){
-  console.log('running');
+  console.log('running, 3000');
 });
