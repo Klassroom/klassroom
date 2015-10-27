587bfac126386eb19c9cb42a0c52c7ec2babc775
Bu Kinoshita <bukinoshita@gmail.com>
2015-10-27 16:21:00 -0400

    Removing connection with DB & Local Auth

--- config/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema({
-  local: {
-    username: String,
-    password: String
-  }
-});
-
-module.exports = mongoose.model('User', Schema);
