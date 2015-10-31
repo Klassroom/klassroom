3966f875bc51d46a0eaf6dd82948e10e244fd826
Bu Kinoshita <bukinoshita@gmail.com>
2015-10-30 22:29:41 -0400

    Adding my courses

---
+++ app/scripts/controllers/dashboardController.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('klassroom')
+    .controller('dashboardController', dashboardController);
+
+  function dashboardController () {
+    
+  };
+})();
